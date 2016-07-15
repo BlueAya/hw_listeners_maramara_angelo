175d0ff9b145b3028d0bf6c54526f106a27cadec
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-15 09:40:06 -0400

    Correct event listener type

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-var submitBtn = document.getElementById('submit');
+// var submitBtn = document.getElementById('submit');
+// var firstName = document.getElementById('firstname');
+// var lastName = document.getElementById('lastname');
+// var email = document.getElementById('email');
+// var message = document.getElementById('message');
+//
+// submitBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('The firstName is: ' + firstName.value);
+//     console.log('The lastName is: ' + lastName.value);
+//     console.log('The email is: ' + email.value);
+//     console.log('The message is: ' + message.value);
+// });
+
+var form = document.getElementById('form');
 var firstName = document.getElementById('firstname');
 var lastName = document.getElementById('lastname');
 var email = document.getElementById('email');
 var message = document.getElementById('message');
 
-submitBtn.addEventListener('click', function (event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     console.log('The firstName is: ' + firstName.value);
     console.log('The lastName is: ' + lastName.value);
